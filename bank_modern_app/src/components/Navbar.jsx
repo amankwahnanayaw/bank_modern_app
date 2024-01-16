ca2aec91293bfa9018ac8402cfbac49d6cb2996a
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-16 19:02:59 +0000

    creating the navbar

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -4,20 +4,29 @@ import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
 const Navbar = () => {
-  return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
-        <img src={ logo } alt="hoobank" className='w-[124px] h-[32px]'/>
+    const [toggle, setToggle] = useState(false);
 
-        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-            {navLinks.map((nav, index) => (
-                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'}text-white`}>
-                    <a href={ `#${nav.id}` }>{ nav.title }</a>
-                </li>
-            ))}
+    return (
+        <nav className='w-full flex py-6 justify-between items-center navbar'>
+            <img src={ logo } alt="hoobank" className='w-[124px] h-[32px]'/>
 
-        </ul>
-    </nav>
-  )
+            <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+                {navLinks.map((nav, index) => (
+                    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'}text-white`}>
+                        <a href={ `#${nav.id}` }>{ nav.title }</a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className='sm:hidden flex flex-1 justify-end items-center'>
+                    <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain' onClick={() => setToggle((prev) => !prev)}/>
+
+                    <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient`}>
+
+                    </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
