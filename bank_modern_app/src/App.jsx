ade54ea06e55c73b82324ced7dc5113345f2ae4c
Nana Yaw_Main <amankwahnanayaw5@gmail.com>
2024-01-16 13:26:07 +0000

    creating app containers

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styles from './styles';
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
-      <div>
-        
+
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
       </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+
+        </div>
+      </div>
+
     </div>
   )
 
